d46888294e68fd31f130926a2a688a64aad2e857
Fearkin <aloneWorker@yandex.ru>
2022-12-26 21:25:12 +0600

    initial commit

---
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,23 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.ui = {
+   theme = "gruvchad",
+}
+
+--[[M.plugins = {
+    options = {
+      lspconfig = {
+        setup_lspconf = "custom.plugins.lspconfig",
+      },
+    },
+}
+]]--
+M.plugins = require "custom.plugins"
+
+
+return M
